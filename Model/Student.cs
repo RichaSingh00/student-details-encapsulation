cb1ae3bbf688ae5ff5c24226d001312c8315b147
RichaSingh00 <ririco613@gmail.com>
2023-09-07 17:37:11 +0530

    Add project files.

---
+++ Model/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp.Model
+{
+    internal class Student
+    {
+        private int _rollno;
+        private string _id;
+        private float _cgpa;
+        public Student()
+        {
+            _cgpa = 0;
+        }
+        //setter methods
+        public void SetRollNo(int rollno)
+        { _rollno = rollno; }
+        public void SetId(string id)
+        { _id = id.Length>=5?id:string.Empty; }
+        public void SetCgpa(float cgpa)
+        { _cgpa = Math.Max(0,Math.Min(10,cgpa)); } //default cgpa is 2: to do
+        //getter methods
+        public int GetRollNo()
+        { return _rollno; }
+        public string GetId()
+        { return _id; }
+        public float GetCgpa()
+        { return _cgpa; }
+        public float CalculatePercentage()
+        {
+            return _cgpa * 10;
+        }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using StudentApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace StudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.SetRollNo(1);
+            student.SetId("Richa");
+            student.SetCgpa(9.25f);
+
+            int rollno =student.GetRollNo();
+            string id=student.GetId();
+            float cgpa=student.GetCgpa();
+
+            Console.WriteLine("Roll No: " + student.GetRollNo());
+            Console.WriteLine("Name: " + student.GetId());
+            Console.WriteLine("CGPA: " + student.GetCgpa());
+            Console.WriteLine("Percentage: " +student.CalculatePercentage());
+        }
+    }
+}
